d6b3219205dc47da2efe3af2136abaa7704d2761
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-09-03 03:16:51 +0000

    updated lambda/handlers/factory.js.

--- lambda/handlers/factory.js
+++ lambda/handlers/factory.js
@@ -1,40 +1,22 @@
-/* *
- * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
- * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
- * session persistence, api calls, and more.
- * */
-const Alexa = require('ask-sdk-core');
-const handlersFactory = require('./handlers/factory');
+import Alexa from 'ask-sdk-model';
+import Messages from '../constants/Messages';
+import Metronome from '../Metronome';
+import IntentsHandlers from './Intents';
+import LifeCyclesHandlers from './LifeCycles';
 
-const {
+function handlersFactory() {
+  const messages = new Messages();
+  const metronome = new Metronome();
+
+  const intentHandlers = new IntentsHandlers(Alexa, messages, metronome);
+  const lifeCyclesHandlers = new LifeCyclesHandlers(Alexa, messages);
+
+
+  return {
     lifeCyclesHandlers,
     intentHandlers
-} = handlersFactory();
-
-const HelloWorldIntentHandler = {
-    canHandle(handlerInput) {
-        console.log({ handlerInput })
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MetronomoIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Hello World!';
-
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
-};
+  };
+}
 
 
-exports.handler = Alexa.SkillBuilders.custom()
-    .addRequestHandlers(
-        lifeCyclesHandlers.launchRequest,
-        intentHandlers.openMetronome,
-    )
-    .addErrorHandlers(
-        lifeCyclesHandlers.error
-    )
-    .withCustomUserAgent('sample/hello-world/v1.2')
-    .lambda();
+export default handlersFactory;
