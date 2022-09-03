36e070c288776cdaccfd9a9983ea60ad9efe8d2e
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-09-03 03:08:27 +0000

    updated lambda/handlers/Intents.js.

---
+++ lambda/handlers/Intents.js
@@ -0,0 +1,20 @@
+class IntentsHandlers {
+  constructor(alexa, messages) {
+    this.alexa = alexa;
+    this.messages = messages;
+  }
+
+  metronome() {
+    return {
+      canHandle(handleInput) {
+        const matchIntent = true;
+        const matchRequestType = this.alexa
+        ;
+
+        return matchIntent && matchRequestType;
+      }
+    }
+  }
+}
+
+module.exports = IntentsHandlers;
