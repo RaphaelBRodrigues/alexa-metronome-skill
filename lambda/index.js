5d6cae07fbbbc4b3022235578bf447bc1b4c3d49
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-09-03 19:09:18 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -4,20 +4,64 @@
  * session persistence, api calls, and more.
  * */
 const Alexa = require('ask-sdk-core');
-const handlersFactory = require('./handlers/factory');
+// const handlersFactory = require('./handlers/factory');
 
-const {
-    lifeCyclesHandlers,
-    intentHandlers
-} = handlersFactory();
+// const {
+//     lifeCyclesHandlers,
+//     intentHandlers
+// } = handlersFactory();
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Welcome, you can say Hello or Help. Which would you like to try?';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const HelloWorldIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Hello World!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
+        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
 
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
-        lifeCyclesHandlers.launchRequest,
-        intentHandlers.openMetronome,
+        LaunchRequestHandler,
+        HelloWorldIntentHandler,
     )
     .addErrorHandlers(
-        lifeCyclesHandlers.error
+        ErrorHandler
     )
     .withCustomUserAgent('sample/hello-world/v1.2')
     .lambda();
