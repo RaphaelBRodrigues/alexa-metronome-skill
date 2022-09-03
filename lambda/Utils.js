027b6ecd4514de520bf0fd9e672893acc326d6bd
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-09-03 20:37:48 +0000

    updated lambda/Utils.js.

--- lambda/Utils.js
+++ lambda/Utils.js
@@ -4,10 +4,15 @@ async function getS3Object(s3ObjectKey) {
     const s3 = new AWS.S3();
     const params = {
         Bucket: process.env.S3_PERSISTENCE_BUCKET,
-        Key: s3ObjectKey
+        Key: s3ObjectKey,
+        prefi
     }
     
-    return await s3.getObject(params, (data, error) => {
+    return await s3.getObject({
+        Bucket: process.env.S3_PERSISTENCE_BUCKET,
+        Key: objectKey,
+        Prefix: "sounds/"
+    }, (data, error) => {
         if (error) {
             return error;
         }
