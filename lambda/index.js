c43bfc73b46bd86dece8b3c8385759891d35b4a0
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-09-03 03:20:10 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -4,59 +4,20 @@
  * session persistence, api calls, and more.
  * */
 const Alexa = require('ask-sdk-core');
+const handlersFactory = require('./handlers/factory');
 
-const HelloWorldIntentHandler = {
-    canHandle(handlerInput) {
-        console.log({ handlerInput })
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MetronomoIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Hello World!';
+const {
+    lifeCyclesHandlers,
+    intentHandlers
+} = handlersFactory();
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
-};
-
-/**
- * Generic error handling to capture any syntax or routing errors. If you receive an error
- * stating the request handler chain is not found, you have not implemented a handler for
- * the intent being invoked or included it in the skill builder below 
- * */
-const ErrorHandler = {
-    canHandle() {
-        return true;
-    },
-    handle(handlerInput, error) {
-        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
-        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
-
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
-};
-
-/**
- * This handler acts as the entry point for your skill, routing all request and response
- * payloads to the handlers above. Make sure any new handlers or interceptors you've
- * defined are included below. The order matters - they're processed top to bottom 
- * */
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
-        LaunchRequestHandler,
-        HelloWorldIntentHandler,
-        HelpIntentHandler,
-        CancelAndStopIntentHandler,
-        FallbackIntentHandler,
-        SessionEndedRequestHandler,
-        IntentReflectorHandler
+        lifeCyclesHandlers.launchRequest,
+        intentHandlers.openMetronome,
     )
     .addErrorHandlers(
-        ErrorHandler)
+        lifeCyclesHandlers.error
+    )
     .withCustomUserAgent('sample/hello-world/v1.2')
     .lambda();
