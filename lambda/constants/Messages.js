7a87d81a2e7ed248e703021e3e5601e9ccb768a5
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-09-03 21:40:01 +0000

    updated lambda/constants/Messages.js.

--- lambda/constants/Messages.js
+++ lambda/constants/Messages.js
@@ -1,6 +1,6 @@
 const Messages = {
-  LAUNCH_MESSAGE: "Iniciando metrônomo",
   REQUEST_ANSWER: "Diga a quantos bpms deseja tocar",
+  LAUNCH_MESSAGE: this.REQUEST_ANSWER,
   ERROR_HANDLER: "Algo deu errado, até mais!",
 }
 
