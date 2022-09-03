53cbe6ce403540695d27775a2e146ab356d1381f
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-09-03 19:50:33 +0000

    updated lambda/handlers/Intents.js.

--- lambda/handlers/Intents.js
+++ lambda/handlers/Intents.js
@@ -1,6 +1,5 @@
 const IntentsHandlers = (alexa, messages, metronome) => {
   const openMetronome = {
-    return {
      canHandle(handlerInput) {
             return alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
                 && alexa.getIntentName(handlerInput.requestEnvelope) === 'MetronomoIntent';
@@ -13,7 +12,6 @@ const IntentsHandlers = (alexa, messages, metronome) => {
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
       }
-    }
   };
   
   return {
