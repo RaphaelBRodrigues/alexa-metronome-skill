c11deceb6a50511e3db293f9c5e0480a1c0201b5
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-09-03 03:06:39 +0000

    updated lambda/handlers/LifeCycles.js.

---
+++ lambda/handlers/LifeCycles.js
@@ -0,0 +1,42 @@
+const Messages = require("../constants/Messages");
+
+class LifeCyclesHandlers {
+  alexa;
+  messages;
+
+  constructor(alexa, messages) {
+    this.alexa = alexa;
+    this.messages = messages;
+  }
+
+  launchRequest() {
+    return {
+      canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+      },
+      handle(handlerInput) {
+        const speakOutput = Messages.LAUNCH_MESSAGE;
+
+        return handlerInput.responseBuilder
+          .speak(speakOutput)
+          .reprompt(speakOutput)
+          .getResponse();
+      }
+    }
+  }
+  error() {
+    return {
+      canHandle() {
+        return true;
+      },
+      handle(handlerInput, error) {
+        console.error(error);
+
+        return handlerInput.responseBuilder
+          .speak(Messages.ERROR_HANDLER)
+      }
+    }
+  }
+}
+
+module.exports = LifeCyclesHandlers;
