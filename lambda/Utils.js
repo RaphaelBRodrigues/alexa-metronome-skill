9ffd367873dfb924b03eb3bde15df1db3ee2df9f
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-09-03 20:18:54 +0000

    updated lambda/Utils.js.

--- lambda/Utils.js
+++ lambda/Utils.js
@@ -2,14 +2,12 @@ const AWS = require('aws-sdk');
 
 async function getS3Object(s3ObjectKey) {
     const s3 = new AWS.S3();
-
     const params = {
-        Bucket: process.env.S3_PERSISTENCE_BUCKET, // Pega Bucket name da constante do ambiente.
-        Key: s3ObjectKey // Caminho para pegar o arquivo TXT.
+        Bucket: process.env.S3_PERSISTENCE_BUCKET,
+        Key: s3ObjectKey
     }
     
     return await s3.getObject(params, (data, error) => {
-        // Em caso de erro, não faça nada. Apenas retorne o erro.
         if (error) {
             return error;
         }
