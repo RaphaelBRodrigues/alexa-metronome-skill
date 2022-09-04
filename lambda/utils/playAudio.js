ea6e24ec2eddd9498d289b2a0127c4eb4ba7bd8e
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-09-04 23:27:05 +0000

    updated lambda/utils/getS3SignedURL.js, lambda/utils/playAudio.js.

--- lambda/utils/playAudio.js
+++ lambda/utils/playAudio.js
@@ -2,10 +2,8 @@ const getS3SignedURL = require('./getS3SignedURL')
 
 async function playAudio(responseBuilder, audio) {
   const url = await getS3SignedURL(audio.s3ObjectKey);
-  console.log(
-      audio
-  );
-  console.log(url)
+  console.log(url);
+  
   return responseBuilder.addAudioPlayerPlayDirective("REPLACE_ALL", url, audio.token, audio.offsetInMilliseconds, null);
 }
 
