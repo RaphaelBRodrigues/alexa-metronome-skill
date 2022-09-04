b6cb7aede44f97a8059d5c70a09999f95c77a92f
RaphaelBRodrigues <raphaelbarbosa.rodrigues@gmail.com>
2022-09-03 23:17:42 -0300

    adjusting s3 signed url function

--- lambda/utils/getS3SignedURL.js
+++ lambda/utils/getS3SignedURL.js
@@ -1,17 +1,17 @@
 const AWS = require('aws-sdk');
 
-const s3SignedURLV4Client = new AWS({
+const s3SignedURLV4Client = new AWS.S3({
     signatureVersion: 'V4',
     region: process.env.S3_PERSISTENCE_REGION
 })
 
 function getS3SignedURL(objectKey) {
     const signedURL = s3SignedURLV4Client.getS3SignedURL("getObject", {
-        Bucket: process.S3_PERSISTENCE_BUCKER,
+        Bucket: process.S3_PERSISTENCE_BUCKET,
         Key: objectKey,
         Expires: 60
     });
-    
+
     return signedURL;
 }
 
