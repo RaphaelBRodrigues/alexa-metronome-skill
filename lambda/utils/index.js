015833e0deab28e2fa8dbead314567e77843a9f1
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-09-04 00:37:23 +0000

    updated lambda/utils/index.js.

--- lambda/utils/index.js
+++ lambda/utils/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   getSlotIntentsValue: require('./getSlotIntentsValue'),
   getS3Object: require('./getS3Object'),
+  getS3SignerURL: require('./getS3SignerURL'),
   playAudio: require('./playAudio'),
 }
