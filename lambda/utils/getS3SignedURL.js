e15bce504167617a00faeceeaebc0a131e7e74bc
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-09-04 02:49:23 +0000

    updated lambda/utils/getS3SignedURL.js.

--- lambda/utils/getS3SignedURL.js
+++ lambda/utils/getS3SignedURL.js
@@ -1,23 +1,22 @@
-const { S3Client, getSignedUrl } = require("aws-sdk");
+const AWS = require("aws-sdk");
 
 const s3 = new AWS.S3({
-    
+
 })
 
 function getS3SignedURL(objectKey) {
     return new Promise((resolve, reject) => {
-    s3.getSignedURL("putObject", {
-        Bucket: process.S3_PERSISTENCE_BUCKET,
-        Key: objectKey,
-        Expires: 60
-    }).then((data) => {
-        resolve(data);
-    }).catch((err) => {
-        reject(err);
+        s3.getSignedURL("putObject", {
+            Bucket: process.S3_PERSISTENCE_BUCKET,
+            Key: objectKey,
+            Expires: 60
+        }).then((data) => {
+            resolve(data);
+        }).catch((err) => {
+            reject(err);
+        })
     })
-    })
-
-    return signedURL;
 }
+
 
 module.exports = getS3SignedURL;
