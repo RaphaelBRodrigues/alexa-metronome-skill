8b099123a41ea4f1d0eaf8fac742222df203657e
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-09-04 01:13:46 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -1,43 +1,19 @@
 const Alexa = require('ask-sdk-core');
-const IntentsLabels = require('./constants/Intents');
-const Messages = require('./constants/Messages');
-const RequestTypes = require('./constants/RequestTypes');
 const handlersFactory = require('./handlers/factory');
 
-// const {
-//     lifeCyclesHandlers,
-//     intentHandlers
-// } = handlersFactory();
+const {
+    lifeCyclesHandlers,
+    intentHandlers
+} = handlersFactory();
 
 
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
-        {
-            canHandle(handlerInput) {
-                return Alexa.getRequestType(handlerInput.requestEnvelope) === RequestTypes.LAUNCH;
-            },
-            handle(handlerInput) {
-                const speakOutput = Messages.REQUEST_BPM;
-
-                return handlerInput.responseBuilder
-                    .speak(speakOutput)
-                    .reprompt(speakOutput)
-                    .getResponse();
-            }
-        }
+        lifeCyclesHandlers.launchRequest,
+        intentHandlers.startMetronome,
     )
     .addErrorHandlers(
-        {
-            canHandle() {
-                return true;
-            },
-            handle(handlerInput, error) {
-                console.error(error);
-
-                return handlerInput.responseBuilder
-                    .speak(Messages.ERROR_HANDLER)
-            }
-        }
+        lifeCyclesHandlers.error
     )
     .withCustomUserAgent('sample/hello-world/v1.2')
     .lambda();
