aa976967ae2b3f051200edf2d4835daf6a11c6eb
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-09-08 00:59:32 +0000

    updated lambda/utils/playAudios.js.

--- lambda/utils/playAudios.js
+++ lambda/utils/playAudios.js
@@ -1,12 +1,13 @@
 const getS3SignedURL = require('./getS3SignedURL')
 
 async function playAudio(responseBuilder, audios) {
-  const urls = await Promise.all(audios.map(async (audio) => {
+  const sound = "https://s3.amazonaws.com/cdn.dabblelab.com/audio/one-small-step-for-man.mp3";
+  const urls = sound || await Promise.all(audios.map(async (audio) => {
       return await getS3SignedURL(audio.s3ObjectKey)
   }));
 
   const audiosTag = urls.map((url) => {
-      return `<audio src="https://s3.amazonaws.com/cdn.dabblelab.com/audio/one-small-step-for-man.mp3" />`; 
+      return `<audio src=${url} />`; 
   }).join(" ");
   
 
