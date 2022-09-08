6542d97df794df8666a2e4f05ad44480e5610b8f
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-09-08 01:05:04 +0000

    updated lambda/utils/playAudios.js.

--- lambda/utils/playAudios.js
+++ lambda/utils/playAudios.js
@@ -5,7 +5,9 @@ async function playAudio(responseBuilder, audios) {
   const urls = await Promise.all(audios.map(async (audio) => {
       return await getS3SignedURL(audio.s3ObjectKey)
   }));
-
+    console.log({
+        urls
+    })
   const audiosTag = urls.map((url) => {
       return `<audio src="${url}" />`; 
   }).join(" ");
