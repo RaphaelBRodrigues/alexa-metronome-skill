e144c66dec76cc95048d00ebd925bef2f7ca7a7f
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-09-08 00:38:59 +0000

    updated lambda/constants/Resources.js.

--- lambda/constants/Resources.js
+++ lambda/constants/Resources.js
@@ -1,12 +1,12 @@
 const Resources = {
     Sounds: {
         tic: {
-            s3ObjectKey: "Media/sounds/tic.mp3",
+            s3ObjectKey: "Media/sounds/tic.ogg",
             token: "tic-sound",
             offsetInMilliseconds: 0,
         },
         tac: {
-            s3ObjectKey: "Media/sounds/tac.mp3",
+            s3ObjectKey: "Media/sounds/tac.ogg",
             token: "tic-sound",
             offsetInMilliseconds: 0,
         }
